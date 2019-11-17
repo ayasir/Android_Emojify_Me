d3e3be9e6031ab4e00d9c4b78e1ea160012a4e00
ayasir <yasirmahmad@gmail.com>
2019-11-17 23:23:08 +0300

    final commit

--- app/src/main/java/com/yasir/androidemojifyme/MainActivity.java
+++ app/src/main/java/com/yasir/androidemojifyme/MainActivity.java
@@ -186,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
         // Resample the saved image to fit the ImageView
         mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
 
+        mResultsBitmap = Emojifier.detectFacesAndOverlayEmoji(this, mResultsBitmap);
         // Set the new bitmap to the ImageView
         mImageView.setImageBitmap(mResultsBitmap);
     }
